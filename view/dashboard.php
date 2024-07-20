a7e3f966f96fae4aa9627fc74c9ff8afa6c1eee7
Med-lemame <168587040+Med-lemame@users.noreply.github.com>
2024-07-20 16:52:09 +0100

    Update dashboard.php

--- view/dashboard.php
+++ view/dashboard.php
@@ -4,7 +4,7 @@ namespace Phppot;
 use \Phppot\Member;
 
 if (! empty($_SESSION["userId"])) {
-    require_once __DIR__ . './../class/Member.php';
+    require_once __DIR__ . './class/Member.php';
     $member = new Member();
     $memberResult = $member->getMemberById($_SESSION["userId"]);
     if(!empty($memberResult[0]["display_name"])) {
